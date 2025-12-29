8135c7d5788dde37a600df7324b6fda57582f273
microxel2006 <pegueraaexel13@gmail.com>
2025-12-29 10:11:28 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const isDark = body.classList.contains("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  toggleBtn.textContent = isDark ? "☀️" : "🌙";
+});
